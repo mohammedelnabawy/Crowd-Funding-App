c107d64ac98752f220a2f71931f399cc82376bc6
mohammedelnabawy <mohammedelnabawy11@gmail.com>
2023-04-04 23:09:06 +0200

    check email

--- Registration.py
+++ Registration.py
@@ -3,66 +3,56 @@ import time
 
 mail_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 name_regex = re.compile(r'^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$')
-mobile_regex = re.compile(r'^01[215]+[0-9]{8}')
+mobile_regex = re.compile(r'^01[0215]+[0-9]{8}')
+pass_reges = re.compile(r'[0-9a-zA-Z]{8,}')
 
 
-def enter_name(name):
+def enter_data(Data_name, Data_regex=''):
     while True:
-        add_name = input(f'Enter the {name}......')
-        if len(add_name) and re.fullmatch(name_regex, add_name):
-            return add_name
+        add_data = input(f'Enter your {Data_name}: ')
+        if len(add_data) and re.fullmatch(Data_regex, add_data):
+            return add_data
         else:
-            print("Enter Valid name ....")
-
-def enter_email(email):
-    while True:
-        add_email = input(f'Enter the {email}......')
-        if len(add_email) and re.fullmatch(mail_regex, add_email):
-            return add_email
-        else:
-            print("Enter Valid email ....")
-
-def enter_mobile(mobile):
-    while True:
-        add_phone = input(f'Enter the {mobile}......')
-        if len(add_phone) and re.fullmatch(mobile_regex, add_phone):
-            return add_phone
-        else:
-            print("Enter Valid mobile ....")
-
-def enter_password(password):
-    while True:
-        add_password = input(f'Enter {password}......')
-        if len(add_password) > 8:
-            return add_password
-        else:
-            print("Enter Valid Password.....")
+            print(f"Invalid {Data_name}....")
 
 def validate_confirm_pass():
     while True:
-        password = enter_password("password")
-        confirm_password = enter_password("Confirm password")
+        password = enter_data("Password", pass_reges)
+        confirm_password = enter_data("Password again", pass_reges)
         if password == confirm_password:
             return password
 
-
 def registeration_user():
     id = round(time.time())
-    f_name = enter_name("First name")
-    l_name = enter_name("Last name")
-    Email = enter_email("Email")
-    mobile = enter_mobile("mobile")
+    f_name = enter_data("First name", name_regex)
+    l_name = enter_data("Last name", name_regex)
+    Email = check_mail()
+    mobile = enter_data("mobile", mobile_regex)
     passwords = validate_confirm_pass()
     return (f'{id}:{f_name}:{l_name}:{Email}:{passwords}:{mobile}')
 
+def check_mail():
+    while True:
+        Email = enter_data("Email", mail_regex)
+        users = read_users()
+        if users == False:
+            return Email
+
+        for user in users:
+            if Email == user.split(':')[3]:
+                print("Email is already exist")
+                break
+        else:
+            return Email
 
 def add_user():
     try:
-        file1 = open("users.txt", "a")
-        file1.write("\n")
+        file1 = open("Data/users.txt", "a")
         file1.write(registeration_user())
+        file1.write("\n")
     except Exception:
         print("unable to open file")
+
 
 
 def login():
@@ -79,12 +69,14 @@ def login():
 
 def read_users():
     try:
-        file1 = open("users.txt", "r")
+        file1 = open("Data/users.txt", "r")
     except Exception:
-        print("unable to open file")
+        return False
     else:
         users = file1.readlines()
         return users
 
-login()
+# login()
 add_user()
+# read_users()
+# registeration_user()
