887cd80beefc9c43edb681ee05d5196683de3131
mohammedelnabawy <mohammedelnabawy11@gmail.com>
2023-04-06 22:52:57 +0200

    project customization v3

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ from colorama import Fore, Back, Style
 login_menu = ["1 - login  ", "2 - Register", "3 - Exit   "]
 project_CRUD_menue = ["1 - Create project  ", "2 - List projects  ", "3 - Edit project   ", "4 - Delete project ", "5 - Exit           "]
 project_menue = ['Id', 'Owner_id', 'Project title', 'Project desc','Project budget', 'Project start date', 'Project End data']
-project_editing_menue = ['1 - Project title', '2 - Project desc','3 - Project budget', '4 - Project start date', '5 - Project End data']
+project_editing_menue = ['1 - Project title    ', '2 - Project desc     ','3 - Project budget    ', '4 - Project start date', '5 - Project End data ']
 
 
 
@@ -18,7 +18,7 @@ def Enter_choice(name):
         if no.isdigit():
             return no
         else:
-            print(f'Invalid {name}')
+            print(Fore.RED + f'\n Invalid {name} \n' + Style.RESET_ALL)
 
 def call_menue(token):
         DisplayData.display_data(["     choose an option    "], project_CRUD_menue)
@@ -49,7 +49,7 @@ def choice(no):
 
 def menue(no, token):
     if(no==1):
-        project_data = project_data = projrct.Create_project(token)
+        project_data = project_data = operations.Create_project(token)
         flag = ReadWriteData.add_data('Data/projects.txt', project_data)
         if flag == True:
             print(Fore.GREEN + '\n   project added successfully \n' + Style.RESET_ALL)
@@ -60,14 +60,20 @@ def menue(no, token):
         call_menue(token)
     elif(no==3):
         DisplayData.display_data(['choose an item'], project_editing_menue)
-        feildnum = Enter_choice('Choice')
-        row_id = Enter_choice('Id')
-        message = projrct.edit_data(int(row_id), int(feildnum), int(token))
+        while True:
+            feildnum = Enter_choice('Choice')
+            if int(feildnum) <= 5 and int(feildnum) >= 1:
+                break
+            else:
+                print(Fore.RED + '\n Invalid Choice \n' + Style.RESET_ALL)
+                
+        row_id = Enter_choice('Project Id')
+        message = operations.edit_data(int(row_id), int(feildnum), int(token))
         print(Fore.GREEN + '\n  ' + message + '\n' + Style.RESET_ALL)
         call_menue(token)
     elif(no==4):
         row_id = Enter_choice('Id')
-        message = projrct.delete_project(int(row_id), int(token))
+        message = operations.delete_project(int(row_id), int(token))
         print(Fore.GREEN + '\n  ' + message + '\n' + Style.RESET_ALL)
         call_menue(token)
     elif(no==5):
